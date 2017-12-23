c87051442ec865b14f1f989842dbc7b5c37fac3b
Eiki Kanaoka <kakaka00eee@yahoo.co.jp>
2017-12-23 09:54:50 +0000

    model/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save {self.email = self.email.downcase}
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
@@ -12,4 +13,26 @@ class User < ApplicationRecord
                               BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    # ランダムなトークンを返す
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+    
+    # 永続的セッションのためにユーザーをデータベースに記憶する
+    def remember(user)
+      user.remember
+      cookies.permanent.signed[:user_id] = user.id
+      cookies.permanent[:remember_token] = user.remember_token
+    end
+    
+    # 渡されたトークンがダイジェストと一致したらtrueを返す
+    def authenticate?(remember_token)
+      Bcrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+  end
+
+>>>>>>>>> local version
 end
