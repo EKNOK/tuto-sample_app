9174b18c604f2cb45757f8e7dad9b2ab77adc5f3
EikiKanaoka <kakakaeee@gmail.com>
2018-01-06 16:18:24 +0900

    User edit,update. correct_user, logged_in_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: 3}
+  validates :password, presence: true, length: {minimum: 3}, allow_nil: true
   has_secure_password
 
   def self.digest(string)
